bf800b362005165c392a09860d4580a8bf1ee5dd
ShrivardhanGoenka <goenkashri@gmail.com>
2021-06-19 17:59:18 +0530

    Initial Commit

--- Main/settings.py
+++ Main/settings.py
@@ -21,7 +21,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 
+SECRET_KEY = 'django-insecure-n#5gopw6(*xh2__i&+t(n!2iusl2x169k7#ou-yl*h(3n$95+l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
